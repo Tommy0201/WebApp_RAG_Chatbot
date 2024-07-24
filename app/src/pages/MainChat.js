ed35d0af777f245c8502a293ae4b998747f24f3d
Tommy0201 <ng.phucminh04@gmail.com>
2024-07-24 15:42:43 +0700

    adding user vs bot message to display on chat container

--- app/src/pages/MainChat.js
+++ app/src/pages/MainChat.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './chat.css';
 
 const MainChat = () => {
+  const [inputMess,setInputMess] = useState("");
+  const [messages,setMessages] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputMess(event.target.value);
+  }
+
+  const handleSendMessage = () => {
+    if (inputMess.trim()) {
+      setMessages([...messages, { sender: 'user', message: inputMess }]);
+      console.log("messages: ",messages)
+      setInputMess("");
+    }
+  }
+
   return (
     <div className="main-chat-container">
       <div className="chat-container">
         {/* Chat messages will go here */}
-        <div className="messages">
-          THIS IS THE MAINCHAT
-        </div>
+        <div className="messages"></div>
+          {messages.map((msg,index)=> (
+            <div key={index} className={`message-item ${msg.sender}-message`}>
+              {msg.message}
+            </div>
+          ))}
+
       </div>
+
       <div className="input-bar">
-        <input type="text" placeholder="Chat with your bot..." />
-        <button>Send</button>
+        <input 
+        type="text" 
+        placeholder="Chat with your bot..." 
+        value={inputMess}
+        onChange={handleInputChange}
+        />
+        <button onClick= {handleSendMessage}>Send</button>
       </div>
     </div>
   )
